77e1e520e0bffaf765f7c42154fed68833bd6ca1
adityogi <73770862+adityogi@users.noreply.github.com>
2022-02-22 14:51:38 +0530

    Updates

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/roost/documentaion-doc2/TailWindCSS-Docs/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/roost/documentaion-doc2/TailWindCSS-Docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/roost/documentaion-doc2/TailWindCSS-Docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/roost/documentaion-doc2/TailWindCSS-Docs/src/css/custom.css'),
+];
